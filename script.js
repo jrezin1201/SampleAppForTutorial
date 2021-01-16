62567c449879735ebf2755ae7ff80cc31488bd0e
Jordan Hill <jordane.hill22@gmail.com>
2021-01-16 12:37:29 -0800

    completed project

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-console.log('first message');
-console.log('second message')
-console.log('first message');
-console.log('second message');
-!
-!
-KeyframeEffect
-console.log('hello';
-)
+const firstWord = ['you are ', 'you will ', "you're family "];
+const secondWord = ['become ', 'soon ', 'on the way '];
+const thirdWord = ['the best', 'rich', 'love'];
+
+console.log(Math.floor(Math.random()*3));
+const firstRandomWord = Math.floor(Math.random()*3);
+const secondRandomWord = Math.floor(Math.random()*3);
+const thirdRandomWord = Math.floor(Math.random()*3);
+
+function fortuneTeller () {
+    console.log(firstWord[firstRandomWord]+ secondWord[secondRandomWord]
+        + thirdWord[thirdRandomWord])
+}
+
+fortuneTeller();
